11fbe222fe8acc80684e43bf60ee92b7c278c6d6
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2020-03-09 18:01:05 +0600

    Update Shortest Path-CM.cpp

--- Shortest Path-CM.cpp
+++ Shortest Path-CM.cpp
@@ -59,46 +59,47 @@ ll Union(ll a, ll b)
     }
 }
 
+void cycle(ll n)
+{
+    ll leng=G[n].size();
+    for(ll i=0; i<leng;i++){
+        ll u=G[n][i].first;
+        if(!visited[u]){
+            visited[u]=1;
+            //cout<<"U N :W "<<u+1<<" "<<n+1<<" "<<G[n][i].second<<endl;
+            dp[n]+=G[n][i].second;
+            if(!status[u]){
+                cycle(u);
+                dp[n]+=dp[u];
+            }
+        }
+    }
+    //cout<<"Serial : "<<n+1<<" "<<dp[n]<<endl;
+}
+
+
 ll Query(ll a, ll b)
 {
     ll u=Find(a);
     ll v=Find(b);
     if(u==v){
         if(status[u]){
-            ///if cycle
-
+            ll ans=avg(dp[a]-dp[b]);
+            ans=min(ans,(dp[u]-ans));
+            return ans;
         }else{
-            ///if not cycle
+            ll ans=avg(dp[a]-dp[b]);
+            return ans;
         }
     }else{
         return -1;
     }
 }
-ll subWeight(ll n)
-{
-
-    ll leng=G[n].size();
-    for(ll i=0; i<leng;i++){
-        ll u=G[n][i].first;
-
-        if(!visited[u]){
-            visited[n]=1;
-            cout<<"U N :W "<<u+1<<" "<<n+1<<" "<<G[n][i].second<<endl;
-            dp[n]=subWeight(u)+G[n][i].second;
-            //dp[u]+=dp[n];
-            //ll weig=G[n][i].second;
-            //ll sc=subWeight(u,weig);
-        }
-    }
-    cout<<"Serial : "<<n+1<<" "<<dp[n]<<endl;
-    return dp[n];
-}
-
 
 int main()
 {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll t;
@@ -120,22 +121,21 @@ int main()
             G[v].push_back({u,w});
             Union(u,v);
         }
+        cout<<"Case "<<T<<":"<<endl;
         ll len=cycleReps.size();
-        cout<<"Len : "<<len<<endl;
+        //cout<<"Len : "<<len<<endl;
         for(ll i=0; i<len; i++){
             ll u=cycleReps[i];
-            ll ww=subWeight(u);
+            cycle(u);
+            //cout<<"Rep : "<<u+1<<endl;
         }
         for(ll i=0; i<n; i++)
         {
             if((!visited[i])&&(With[i]==1)){
-                cout<<"Loop : "<<i+1<<endl;
-                ll ww=subWeight(i);
+                //cout<<"Loop : "<<i+1<<endl;
                 visited[i]=1;
+                cycle(i);
             }
-        }
-        for(ll i=0; i<n; i++){
-            cout<<"DP : "<<i+1<<" "<<With[i]<<" "<<dp[i]<<endl;
         }
         ///Query Part
         ll q;
