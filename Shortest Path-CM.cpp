af908b09674597076091292e7acad1d5ec7fadd2
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2020-03-09 13:36:30 +0600

    Update Shortest Path-CM.cpp

--- Shortest Path-CM.cpp
+++ Shortest Path-CM.cpp
@@ -2,14 +2,36 @@
 using namespace std;
 #define ll long long
 
-vector<pair<ll, pair<ll,ll> > >Edges; //first->u, first.first->v, first.second->w
-vector<ll>G[30009]; //
-ll parent[30009];
-map<ll,ll>status,sum;
-map<pair<ll,ll>,ll>weight;
-vector<ll>cycleReps;
-ll visited[30009];
-ll dp[30009]; /// sum of the weight of the cycle.
+vector<pair<ll,ll> >G[30009]; ///keeps the weighted graph
+vector<ll>cycleReps; ///representatoor of cycles
+
+ll parent[30009]; ///keeps the parents of each node
+ll visited[30009]; ///node visit
+ll dp[30009]; /// sum of the weight of each node
+
+map<ll,ll>status; ///sub graph is cycle or not associated with the representator
+map<ll,ll>With; ///number of connection of each node
+
+
+void reset(ll n)
+{
+    cycleReps.clear();
+    status.clear();
+    With.clear();
+    for(ll i=0; i<=n; i++)
+    {
+        parent[i]=0;
+        visited[i]=0;
+        dp[i]=0;
+        G[i].clear();
+    }
+}
+
+ll avg(ll n)
+{
+    if(n>=0)return n;
+    else return -n;
+}
 
 void makeSet(ll n)
 {
@@ -37,25 +59,16 @@ ll Union(ll a, ll b)
     }
 }
 
-ll dfs(ll a, ll b)
-{
-    if(a==b) return 0;
-    ll len=
-}
-
 ll Query(ll a, ll b)
 {
     ll u=Find(a);
     ll v=Find(b);
     if(u==v){
         if(status[u]){
-            ll ans1=sum[u];
-            ll ans2=dfs(a,b);
-            return min(ans1,ans2);
+            ///if cycle
 
         }else{
-            ll ans=dfs(a,b);
-            return ans;
+            ///if not cycle
         }
     }else{
         return -1;
@@ -63,21 +76,29 @@ ll Query(ll a, ll b)
 }
 ll subWeight(ll n)
 {
-    visited[n]=1;
-    ll ct=0;
+
     ll leng=G[n].size();
     for(ll i=0; i<leng;i++){
-        ll u=G[n][i];
+        ll u=G[n][i].first;
+
         if(!visited[u]){
-            return dp[n]=dp[n]+weight[make_pair(n,u)]+subWeight(u);
+            visited[n]=1;
+            cout<<"U N :W "<<u+1<<" "<<n+1<<" "<<G[n][i].second<<endl;
+            dp[n]=subWeight(u)+G[n][i].second;
+            //dp[u]+=dp[n];
+            //ll weig=G[n][i].second;
+            //ll sc=subWeight(u,weig);
         }
     }
+    cout<<"Serial : "<<n+1<<" "<<dp[n]<<endl;
     return dp[n];
 }
 
 
 int main()
 {
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll t;
@@ -85,30 +106,42 @@ int main()
     for(ll T=1; T<=t; T++){
         ll n, e;
         cin>>n>>e;
+        reset(n);
         for(ll i=0; i<=n; i++){
-            makeset(i);
+            makeSet(i);
         }
         for(ll i=0; i<e; i++){
             ll u,v,w;
             cin>>u>>v>>w;
             u--;v--;
-            G[u].push_back(v);
-            G[v].push_back(u);
-            weight[make_pair(u,v)]=w;
-            weight[make_pair(v,u)]=w;
+            With[u]++;
+            With[v]++;
+            G[u].push_back({v,w});
+            G[v].push_back({u,w});
             Union(u,v);
         }
         ll len=cycleReps.size();
+        cout<<"Len : "<<len<<endl;
         for(ll i=0; i<len; i++){
             ll u=cycleReps[i];
             ll ww=subWeight(u);
-            sum[cycleReps[i]]=ww;
+        }
+        for(ll i=0; i<n; i++)
+        {
+            if((!visited[i])&&(With[i]==1)){
+                cout<<"Loop : "<<i+1<<endl;
+                ll ww=subWeight(i);
+                visited[i]=1;
+            }
+        }
+        for(ll i=0; i<n; i++){
+            cout<<"DP : "<<i+1<<" "<<With[i]<<" "<<dp[i]<<endl;
         }
         ///Query Part
         ll q;
         cin>>q;
         for(ll i=0; i<q; i++){
-            ll L, ll R;
+            ll L, R;
             cin>>L>>R;
             L--;R--;
             ll ans=Query(L,R);
